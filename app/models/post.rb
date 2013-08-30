35e4a80ae3547941e823a7bedc7778ffe1cabd82
Mark Chavez <los_populares@yahoo.com>
2013-08-30 18:35:28 +0800

    validate content type

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,13 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :avatar
 
   has_attached_file :avatar, styles: { medium:"300x300>", thumb:"250x250>" }
+  
 
   belongs_to :user
 
   validates :content, presence: true
   validates :content, length: { maximum: 100 }
+  validates_attachment :avatar, content_type: { content_type: /^image\/(png|gif|jpg|jpeg)/ }
 
   default_scope order("created_at DESC")
 end
